c18f486e9a8bbf1c9a97cbea94d0c42281e5cb09
Edith Hohner <edith@edith-ROG-Zephyrus-G14.edith>
2026-08-03 22:38:09 -0500

    works now

--- scr/displaypopup.js
+++ scr/displaypopup.js
@@ -1,7 +1,4 @@
-// content script that displays the popup when the mouse is hovered over a professor name 
-
-// tracks if displayStats() is currently running
-let running = false;
+// content script that displays the popup when the mouse is hovered over a professor name
 
 // start the timer and call refresh display
 const profInterval = setInterval(() => {
@@ -9,37 +6,33 @@ const profInterval = setInterval(() => {
 }, 1500);
 
 // method that decides when to call displayStats()
-async function refreshDisplay(){
-    const searchResults = document.querySelector('[id="search-again"]');
-    if(searchResults.style.length == 1 && !running) {
-        displayStats();
-        running = true;
-    }
-    // let the method run again
-    if(searchResults.style.length == 0){
-        running = false;
-    }
+async function refreshDisplay() {
+  const searchResults = document.querySelector('.results-out-of');
+
+  if (searchResults != null && searchResults.textContent.includes("Class")) {
+    displayStats();
+  }
 }
 
 // get and show ratemyprof stats for each page
 async function displayStats() {
-
     // get all the instructor nodes
     const hoverElements = document.querySelectorAll('[data-property="instructor"]');
 
-    if(hoverElements.length > 1){
+    if (hoverElements.length > 1) {
 
         // listeners to display popup
         for(let i = 1; i < hoverElements.length; i++){
 
             hoverElements[i].addEventListener('mouseenter',
                 () => {
-  
-                    let hover = document.querySelectorAll( ":hover" );
 
+                    let hover = document.querySelectorAll( ":hover" );
                     let professorLink = hover[hover.length - 1];
-                        
-                    // make sure we grabbed the email
+
+                    // get all emails
+                    //  professorLink.querySelector("[class=email]");
+
                     if(professorLink.childNodes.length > 1){
                         professorLink = hover[hover.length - 1].firstChild;
                     }
@@ -47,62 +40,74 @@ async function displayStats() {
                     let professorName = professorLink.innerText;
 
                     // event listening
-
-                    if(professorName != "" && professorName != null) { 
+                  if (professorName != "" && professorName != null) {
 
                         let port = browser.runtime.connect({ name: 'professor-rating' });
-                        port.postMessage({ professorName });   
+                        port.postMessage({ professorName });
+                      port.onMessage.addListener((professor) => {
 
-                        port.onMessage.addListener((professor) => {
+                        if (professor.length != 0) {
 
-                            if(professor.length != 0){
-                                
-                                // insert the professor's stats, create new elements
-
+                              // insert the professor's stats, create new elements
+                              if (document.querySelector("[id=rmp-link]") == null) {
                                 let rmpLink = document.createElement("a");
+                                rmpLink.id = "rmp-link";
                                 rmpLink.className = 'rating'
                                 rmpLink.target = "_blank";
                                 rmpLink.rel = "noopener noreferrer";
                                 rmpLink.href = `https://www.ratemyprofessors.com/professor?tid=${professor.legacyId}`;
                                 rmpLink.innerText = `${professor.numRatings} ratings`;
                                 professorLink.insertAdjacentElement('afterend', rmpLink);
+                              }
 
-                                
+                              if (document.querySelector("[id=avg-rating]") == null) {
                                 let avgRating = document.createElement("div");
+                                avgRating.id = "avg-rating";
                                 avgRating.className = 'rating';
                                 avgRating.innerText = ` ${professor.avgRating} / 5`;
                                 professorLink.insertAdjacentElement('afterend', avgRating);
+
                                 let ratingText = document.createElement("b");
                                 ratingText.innerText = `Rating:`;
                                 avgRating.insertAdjacentElement('afterbegin', ratingText);
+                              }
 
+                              if (document.querySelector("[id=avg-difficulty]") == null) {
                                 let avgDifficulty = document.createElement("div");
                                 avgDifficulty.className = 'rating'
+                                avgDifficulty.id = "avg-difficulty";
                                 avgDifficulty.innerText = ` ${professor.avgDifficulty} / 5`;
                                 professorLink.insertAdjacentElement('afterend', avgDifficulty);
+
                                 let difficultyText = document.createElement("b");
                                 difficultyText.innerText = `Difficulty:`;
                                 avgDifficulty.insertAdjacentElement('afterbegin', difficultyText);
+                              }
 
-                                if (professor.wouldTakeAgainPercent != -1){
-                                    let wouldTakeAgainPercent = document.createElement("div");
-                                    wouldTakeAgainPercent.className = 'rating';
-                                    wouldTakeAgainPercent.innerText = ` would take again.`;
-                                    professorLink.insertAdjacentElement('afterend', wouldTakeAgainPercent);
-                                    let takeAgainText = document.createElement("b");
-                                    takeAgainText.innerText = `${Math.round(Number(professor.wouldTakeAgainPercent))}%`;
-                                    wouldTakeAgainPercent.insertAdjacentElement('afterbegin', takeAgainText);
-                                }   
+                              if (professor.wouldTakeAgainPercent != -1 && document.querySelector("[id=would-take-again]") == null){
+                                let wouldTakeAgainPercent = document.createElement("div");
+                                wouldTakeAgainPercent.id = "would-take-again";
+                                wouldTakeAgainPercent.className = 'rating';
+                                wouldTakeAgainPercent.innerText = ` would take again.`;
+                                professorLink.insertAdjacentElement('afterend', wouldTakeAgainPercent);
+
+                                let takeAgainText = document.createElement("b");
+                                takeAgainText.innerText = `${Math.round(Number(professor.wouldTakeAgainPercent))}%`;
+                                wouldTakeAgainPercent.insertAdjacentElement('afterbegin', takeAgainText);
+                              }
 
                             } else {
-                                let noRatings = document.createElement("div");
-                                noRatings.className = 'rating';
-                                professorLink.insertAdjacentElement('afterend', noRatings);
-                                let ratingText = document.createElement("b");
-                                ratingText.innerText = `No ratings found.`;
-                                noRatings.insertAdjacentElement('afterbegin', ratingText);
-                            } 
-                            
+                                if (document.querySelector("[id=no-ratings]") == null) {
+                                  let noRatings = document.createElement("div");
+                                  noRatings.id = "no-ratings";
+                                  noRatings.className = 'rating';
+                                  professorLink.insertAdjacentElement('afterend', noRatings);
+
+                                  let ratingText = document.createElement("b");
+                                  ratingText.innerText = `No ratings found.`;
+                                  noRatings.insertAdjacentElement('afterbegin', ratingText);
+                                }
+                            }
                         });
                     }
 
@@ -118,7 +123,7 @@ async function displayStats() {
                             professorStats[j].remove();
                         }
                     });
-                
+
                 });
         }
     }
